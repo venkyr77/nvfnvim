13afd7ef3e8333ca99a0d4f8de842db56cf67d01
Venkatesan Ravi <venkyrocker7777@gmail.com>
2025-03-27 06:41:05 +0000

    simplify flake

--- flake.nix
+++ flake.nix
@@ -1,11 +1,21 @@
 {
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nvf = {
       url = "github:notashelf/nvf";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-parts.follows = "flake-parts";
+        flake-utils.follows = "flake-utils";
+        systems.follows = "systems";
+      };
     };
+    systems.url = "github:nix-systems/default";
   };
   outputs = {
     flake-parts,
