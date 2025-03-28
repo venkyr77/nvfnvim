7374554f53e40cc5db21485311e302e8771edda8
Venkatesan Ravi <venkyrocker7777@gmail.com>
2025-03-28 09:32:37 +0000

    close neo tree on dap ui init

--- modules/debug/default.nix
+++ modules/debug/default.nix
@@ -1,6 +1,28 @@
-{
-  config.vim.debugger.nvim-dap = {
-    enable = true;
-    ui.enable = true;
+{lib, ...}: let
+  inherit (lib.nvim.dag) entryAfter;
+in {
+  config.vim = {
+    debugger.nvim-dap = {
+      enable = true;
+      ui = {
+        autoStart = false;
+        enable = true;
+      };
+    };
+    pluginRC.nvim-dap-ui =
+      entryAfter ["nvim-dap"]
+      # lua
+      ''
+        dap.listeners.after.event_initialized["dapui_config"] = function()
+          require("neo-tree").close_all()
+          require("dapui").open()
+        end
+        dap.listeners.before.event_terminated["dapui_config"] = function()
+          require("dapui").close()
+        end
+        dap.listeners.before.event_exited["dapui_config"] = function()
+          require("dapui").close()
+        end
+      '';
   };
 }
