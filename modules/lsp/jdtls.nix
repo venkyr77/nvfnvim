aedf8d13c9d6f138255b78d8c0408f11e5d6b6d7
Venkatesan Ravi <venkyrocker7777@gmail.com>
2025-03-29 17:43:31 +0000

    refactor jdtls

--- modules/lsp/jdtls.nix
+++ modules/lsp/jdtls.nix
@@ -6,6 +6,7 @@
   inherit (lib.generators) mkLuaInline;
   inherit (lib.strings) hasSuffix;
   inherit (lib.nvim.lua) toLuaObject;
+  inherit (import ../../helpers) mkFnBindingToBuffer;
 
   vscode-java-debug = "${pkgs.vscode-extensions.vscjava.vscode-java-debug}/share/vscode/extensions/vscjava.vscode-java-debug";
   vscode-java-test = "${pkgs.vscode-extensions.vscjava.vscode-java-test}/share/vscode/extensions/vscjava.vscode-java-test";
@@ -24,6 +25,41 @@
       (getJars vscode-java-test)
     ])
   );
+
+  settings = {
+    java = {
+      completion.enabled = true;
+      configuration.runtimes = [
+        {
+          name = "JavaSE-1.8";
+          path = "${pkgs.jdk8}";
+        }
+        {
+          name = "JavaSE-11";
+          path = "${pkgs.jdk11}";
+        }
+        {
+          name = "JavaSE-17";
+          path = "${pkgs.jdk17}";
+          default = true;
+        }
+        {
+          name = "JavaSE-21";
+          path = "${pkgs.jdk21}";
+        }
+      ];
+      contentProvider.preferred = "fernflower";
+      eclipse.downloadSources = true;
+      inlayHints.parameterNames.enabled = "all";
+      signatureHelp.enabled = true;
+      sources = {
+        organizeImports = {
+          starThreshold = 9999;
+          staticStarThreshold = 9999;
+        };
+      };
+    };
+  };
 in {
   config.vim = {
     autocmds = [
@@ -49,22 +85,11 @@ in {
               local on_attach = function(_, bufnr)
                 vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
 
-                local overrides = {
-                  -- https://github.com/microsoft/vscode-java-debug/blob/main/src/launchCommand.ts#L27
-                  shortenCommandLine = "argfile",
-                }
+                -- https://github.com/microsoft/vscode-java-debug/blob/main/src/launchCommand.ts#L27
+                local overrides = { shortenCommandLine = "argfile" }
 
-                vim.keymap.set("n", "<leader>ltc", function()
-                  jdtls.test_class({
-                    config_overrides = overrides,
-                  })
-                end, { buffer = bufnr, desc = "[t]est [c]lass" })
-
-                vim.keymap.set("n", "<leader>ltm", function()
-                  jdtls.test_nearest_method({
-                    config_overrides = overrides,
-                  })
-                end, { buffer = bufnr, desc = "[t]est nearest [m]ethod" })
+                ${mkFnBindingToBuffer "<leader>ltc" "function() jdtls.test_class({ config_overrides = overrides }) end" "[t]est [c]lass"}
+                ${mkFnBindingToBuffer "<leader>ltm" "function() jdtls.test_nearest_method({ config_overrides = overrides }) end" "[t]est [c]lass"}
               end
 
               local init_options = {
@@ -77,50 +102,13 @@ in {
                 ),
               }
 
-              local settings = {
-                java = {
-                  completion = { enabled = true },
-                  configuration = {
-                    runtimes = {
-                      {
-                        name = "JavaSE-1.8",
-                        path = "${pkgs.jdk8}",
-                      },
-                      {
-                        name = "JavaSE-11",
-                        path = "${pkgs.jdk11}",
-                      },
-                      {
-                        name = "JavaSE-17",
-                        path = "${pkgs.jdk17}",
-                        default = true,
-                      },
-                      {
-                        name = "JavaSE-21",
-                        path = "${pkgs.jdk21}",
-                      },
-                    },
-                  },
-                  contentProvider = { preferred = "fernflower" },
-                  eclipse = { downloadSources = true },
-                  inlayHints = { parameterNames = { enabled = "all" } },
-                  signatureHelp = { enabled = true },
-                  sources = {
-                    organizeImports = {
-                      starThreshold = 9999,
-                      staticStarThreshold = 9999,
-                    },
-                  },
-                },
-              }
-
               jdtls.start_or_attach({
                 capabilities = capabilities,
                 cmd = cmd,
                 on_attach = on_attach,
                 init_options = init_options,
                 root_dir = root_dir,
-                settings = settings,
+                settings = ${toLuaObject settings},
               })
             end
           '';
@@ -130,8 +118,6 @@ in {
       }
     ];
 
-    extraPlugins."jdtls" = {
-      package = pkgs.vimPlugins.nvim-jdtls;
-    };
+    extraPlugins."jdtls".package = pkgs.vimPlugins.nvim-jdtls;
   };
 }
