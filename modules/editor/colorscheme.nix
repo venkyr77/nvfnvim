aa0dcedd2706204fe0cfcc7e31fd70d14276d2ce
Venkatesan Ravi <venkyrocker7777@gmail.com>
2025-04-04 01:09:46 +0000

    simplify colorscheme as catppuccin integration for lspsaga has been merged upstream - https://github.com/NotAShelf/nvf/pull/779

--- modules/editor/colorscheme.nix
+++ modules/editor/colorscheme.nix
@@ -8,13 +8,6 @@ in {
       style = "mocha";
     };
 
-    lsp.lspsaga.setupOpts.ui.kind =
-      mkLuaInline
-      # lua
-      ''
-        require("catppuccin.groups.integrations.lsp_saga").custom_kind()
-      '';
-
     tabline.nvimBufferline.setupOpts.highlights =
       mkLuaInline
       # lua
