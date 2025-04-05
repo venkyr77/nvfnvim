f820edcbf50aca8bbcde8f2e61126b3342ac80ad
Venkatesan Ravi <venkyrocker7777@gmail.com>
2025-04-05 01:49:46 +0000

    remove indentscope disable autocmd as it is mergerd upstream - https://github.com/NotAShelf/nvf/pull/793

--- modules/autocmds/default.nix
+++ modules/autocmds/default.nix
@@ -8,25 +8,6 @@ in {
         # lua
         ''
           function()
-            local ignore_filetypes = {
-              "help",
-              "neo-tree",
-              "notify",
-            }
-            if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
-              vim.b.miniindentscope_disable = true
-            end
-          end
-        '';
-      desc = "Disable indentscope for certain filetypes";
-      event = ["FileType"];
-    }
-    {
-      callback =
-        mkLuaInline
-        # lua
-        ''
-          function()
             require("lint").try_lint()
           end
         '';
