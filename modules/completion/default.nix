17b4d7975947e88fa69fa012bff859102e53604e
Venkatesan Ravi <venkyrocker7777@gmail.com>
2025-04-06 13:29:17 +0000

    completion fixes

--- modules/completion/default.nix
+++ modules/completion/default.nix
@@ -1,6 +1,12 @@
-{
+{lib, ...}: {
   config.vim.autocomplete.nvim-cmp = {
     enable = true;
     setupOpts.completion.completeopt = "menu,menuone,noselect";
+    sources = lib.mkForce {
+      buffer = "[Buffer]";
+      luasnip = "[LuaSnip]";
+      nvim_lsp = "[LSP]";
+      path = "[Path]";
+    };
   };
 }
