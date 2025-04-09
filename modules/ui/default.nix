3f7627165a7fa81f943919eca3ba2164bf294ee9
Venkatesan Ravi <venkyrocker7777@gmail.com>
2025-04-09 23:40:58 +0000

    lualine & bufferline integrations for snacks

--- modules/ui/default.nix
+++ modules/ui/default.nix
@@ -1,11 +1,36 @@
-{
+{lib, ...}: let
+  inherit (lib.generators) mkLuaInline;
+in {
   config.vim = {
     notes.todo-comments.enable = true;
     statusline.lualine = {
       enable = true;
-      setupOpts.extensions = ["neo-tree"];
+      setupOpts.extensions = [
+        {
+          sections = {
+            lualine_a = mkLuaInline ''
+              {
+                function()
+                  return vim.fn.fnamemodify(vim.fn.getcwd(), ":~")
+                end,
+              }
+            '';
+          };
+          filetypes = ["snacks_picker_list" "snacks_picker_input"];
+        }
+      ];
     };
-    tabline.nvimBufferline.enable = true;
+    tabline.nvimBufferline = {
+      enable = true;
+      setupOpts.options.offsets = [
+        {
+          filetype = "snacks_layout_box";
+          text = "File Explorer";
+          highlight = "Directory";
+          separator = true;
+        }
+      ];
+    };
     ui = {
       borders.enable = true;
       noice = {
